3c4fa26b50c56001f66e755ddddec737cd769957
Vladimer52 <vladim.mackarow2012@yandex.ru>
2021-12-07 18:27:09 +0300

    Добавьте файлы проекта.

---
+++ UpdateService/TimerUdateDb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UpdateService
+{
+    class TimerUdateDb
+    {
+        public static Timer timer;
+        private readonly int _period = 20000; //20s
+        private readonly int _dueTime = 1000; //start after 1s
+
+        public TimerUdateDb(int period, int dueTime)
+        {
+            _period = period;
+            _dueTime = dueTime;
+        }
+        public void Start()
+        {
+            timer = new Timer(
+                        callback: new TimerCallback(TimerTask),
+                        state: null,
+                        dueTime: _dueTime,
+                        period:  _period);
+
+            //replace to SQL
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}: done.");
+        }
+
+        public void Stop() => timer.Dispose();
+
+
+        private static void TimerTask(object timerState)
+    {
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}: starting a new callback.");
+    }
+}
+    }
+
---
+++ UpdateService/UpdateService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UpdateService
+{
+    public partial class UpdateService : ServiceBase
+    {
+        TimerUdateDb timerUdateDb;
+        public UpdateService()
+        {
+            InitializeComponent();
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            timerUdateDb = new TimerUdateDb(20000, 1000);
+        }
+
+        protected override void OnStart(string[] args) => timerUdateDb.Start();
+
+        protected override void OnStop() => timerUdateDb.Stop();
+    }
+}
